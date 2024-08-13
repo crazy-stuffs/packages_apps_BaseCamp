xiaomi-sm8450-marble/android_packages_apps_Personalizations

Lockscreen widgets: Add Bt, Wifi, Data, Ringer toggle widgets
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/settings/preferences/ui/LockscreenWidgetsPreview.java
@@ -1,0 +1,22 @@
+package com.android.settings.preferences.ui;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import com.android.settings.R;
+
+public class LockscreenWidgetsPreview extends LinearLayout {
+
+    public LockscreenWidgetsPreview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int previewHeight = (int) getResources().getDimension(R.dimen.lockscreen_preview_height_with_no_widgets);
+        int newHeightMeasureSpec = MeasureSpec.makeMeasureSpec(previewHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, newHeightMeasureSpec);
+    }
+}

crazy-stuffs/packages_apps_BaseCamp

src/com/everest/basecamp/preferences/ui/LockscreenWidgetsPreview.java
@@ -1,0 +1,22 @@
+package com.everest.basecamp.preferences.ui;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import com.android.settings.R;
+
+public class LockscreenWidgetsPreview extends LinearLayout {
+
+    public LockscreenWidgetsPreview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int previewHeight = (int) getResources().getDimension(R.dimen.lockscreen_preview_height_with_no_widgets);
+        int newHeightMeasureSpec = MeasureSpec.makeMeasureSpec(previewHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, newHeightMeasureSpec);
+    }
+}