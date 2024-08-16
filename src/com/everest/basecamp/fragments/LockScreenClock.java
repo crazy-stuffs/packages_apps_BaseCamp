xiaomi-sm8450-marble/android_packages_apps_Personalizations

lockscreen: Custom AOD image display [1/2]
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/crdroid/settings/fragments/LockScreenClock.java
@@ -15,10 +15,16 @@
  */
 package com.crdroid.settings.fragments;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.View;
@@ -38,6 +44,8 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.widget.LayoutPreference;
 
+import com.crdroid.settings.utils.ImageUtils;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -48,6 +56,9 @@ import java.util.stream.Collectors;
 public class LockScreenClock extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "LockScreenClock";
+    
+    private static final String CUSTOM_IMAGE_REQUEST_CODE_KEY = "lockscreen_custom_image";
+    private static final int CUSTOM_IMAGE_REQUEST_CODE = 1001;
 
     private static final String MAIN_WIDGET_1_KEY = "main_custom_widgets1";
     private static final String MAIN_WIDGET_2_KEY = "main_custom_widgets2";
@@ -68,6 +79,7 @@ public class LockScreenClock extends SettingsPreferenceFragment implements Prefe
     private Preference mExtraWidget4;
     private Preference mDeviceInfoWidgetPref;
     private Button mApplyChange;
+    private Preference mCustomImagePreference;
     
     private SwitchPreferenceCompat mLockScreenWidgetsEnabledPref;
     private List<Preference> mWidgetPreferences;
@@ -98,6 +110,44 @@ public class LockScreenClock extends SettingsPreferenceFragment implements Prefe
         loadInitialPreferences();
         saveInitialPreferences();
         mApplyChange.setEnabled(false);
+        
+        mCustomImagePreference = findPreference(CUSTOM_IMAGE_REQUEST_CODE_KEY);
+        int clockStyle = Settings.System.getIntForUser(getContext().getContentResolver(), "clock_style", 0, UserHandle.USER_CURRENT);
+        String imagePath = Settings.System.getString(getContext().getContentResolver(), "custom_aod_image_uri");
+        if (imagePath != null && clockStyle > 0) {
+            mCustomImagePreference.setSummary(imagePath);
+            mCustomImagePreference.setEnabled(true);
+        } else if (clockStyle == 0) {
+            mCustomImagePreference.setSummary(getContext().getString(R.string.custom_aod_image_not_supported));
+            mCustomImagePreference.setEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mCustomImagePreference) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.setType("image/*");
+            startActivityForResult(intent, CUSTOM_IMAGE_REQUEST_CODE);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        super.onActivityResult(requestCode, resultCode, result);
+        if (requestCode == CUSTOM_IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK && result != null) {
+            Uri imgUri = result.getData();
+            if (imgUri != null) {
+                String savedImagePath = ImageUtils.saveImageToInternalStorage(getContext(), imgUri, "lockscreen_aod_image", "LOCKSCREEN_CUSTOM_AOD_IMAGE");
+                if (savedImagePath != null) {
+                    ContentResolver resolver = getContext().getContentResolver();
+                    Settings.System.putStringForUser(resolver, "custom_aod_image_uri", savedImagePath, UserHandle.USER_CURRENT);
+                    mCustomImagePreference.setSummary(savedImagePath);
+                }
+            }
+        }
     }
 
     private void initializePreferences() {

src/com/crdroid/settings/fragments/WallpaperDepth.java
@@ -43,16 +43,9 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import com.crdroid.settings.utils.ImageUtils;
+
 import java.util.List;
-import java.util.Locale;
 
 @SearchIndexable
 public class WallpaperDepth extends SettingsPreferenceFragment
@@ -102,50 +95,12 @@ public class WallpaperDepth extends SettingsPreferenceFragment
 
             final Uri imgUri = result.getData();
             if (imgUri != null) {
-                String savedImagePath = saveImageToInternalStorage(getContext(), imgUri);
+                String savedImagePath = ImageUtils.saveImageToInternalStorage(getContext(), imgUri, "depthwallpaper", "DEPTH_WALLPAPER_SUBJECT");
                 if (savedImagePath != null) {
                     ContentResolver resolver = getContext().getContentResolver();
                     Settings.System.putStringForUser(resolver, "depth_wallpaper_subject_image_uri", savedImagePath, UserHandle.USER_CURRENT);
                 }
             }
-        }
-    }
-
-    private String saveImageToInternalStorage(Context context, Uri imgUri) {
-        try {
-            InputStream inputStream;
-            if (imgUri.toString().startsWith("content://com.google.android.apps.photos.contentprovider")) {
-                List<String> segments = imgUri.getPathSegments();
-                if (segments.size() > 2) {
-                    String mediaUriString = URLDecoder.decode(segments.get(2), StandardCharsets.UTF_8.name());
-                    Uri mediaUri = Uri.parse(mediaUriString);
-                    inputStream = context.getContentResolver().openInputStream(mediaUri);
-                } else {
-                    throw new FileNotFoundException("Failed to parse Google Photos content URI");
-                }
-            } else {
-                inputStream = context.getContentResolver().openInputStream(imgUri);
-            }
-            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
-            String imageFileName = "DEPTH_WALLPAPER_SUBJECT_" + timeStamp + ".png";
-            File directory = new File("/sdcard/depthwallpaper");
-            if (!directory.exists() && !directory.mkdirs()) {
-                return null;
-            }
-            File[] files = directory.listFiles((dir, name) -> name.startsWith("DEPTH_WALLPAPER_SUBJECT_") && name.endsWith(".png"));
-            if (files != null) {
-                for (File file : files) {
-                    file.delete();
-                }
-            }
-            File file = new File(directory, imageFileName);
-            try (FileOutputStream outputStream = new FileOutputStream(file)) {
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-            }
-            return file.getAbsolutePath();
-        } catch (Exception e) {
-            return null;
         }
     }
 

src/com/crdroid/settings/utils/ImageUtils.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2023-2024 the risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class ImageUtils {
+
+    public static String saveImageToInternalStorage(Context context, Uri imgUri, String featurePath, String filePrefix) {
+        try {
+            InputStream inputStream;
+            if (imgUri.toString().startsWith("content://com.google.android.apps.photos.contentprovider")) {
+                List<String> segments = imgUri.getPathSegments();
+                if (segments.size() > 2) {
+                    String mediaUriString = URLDecoder.decode(segments.get(2), StandardCharsets.UTF_8.name());
+                    Uri mediaUri = Uri.parse(mediaUriString);
+                    inputStream = context.getContentResolver().openInputStream(mediaUri);
+                } else {
+                    throw new FileNotFoundException("Failed to parse Google Photos content URI");
+                }
+            } else {
+                inputStream = context.getContentResolver().openInputStream(imgUri);
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+            String imageFileName = filePrefix + "_" + timeStamp + ".png";
+            File directory = new File("/sdcard/risingOS/" + featurePath);
+            if (!directory.exists() && !directory.mkdirs()) {
+                return null;
+            }
+            File[] files = directory.listFiles((dir, name) -> name.startsWith(filePrefix) && name.endsWith(".png"));
+            if (files != null) {
+                for (File file : files) {
+                    file.delete();
+                }
+            }
+            File file = new File(directory, imageFileName);
+            try (FileOutputStream outputStream = new FileOutputStream(file)) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            }
+            return file.getAbsolutePath();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}

crazy-stuffs/packages_apps_BaseCamp

src/com/everest/basecamp/fragments/LockScreenClock.java
@@ -15,10 +15,16 @@
  */
 package com.everest.basecamp.fragments;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.View;
@@ -38,6 +44,8 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.widget.LayoutPreference;
 
+import com.everest.basecamp.utils.ImageUtils;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -48,6 +56,9 @@ import java.util.stream.Collectors;
 public class LockScreenClock extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "LockScreenClock";
+    
+    private static final String CUSTOM_IMAGE_REQUEST_CODE_KEY = "lockscreen_custom_image";
+    private static final int CUSTOM_IMAGE_REQUEST_CODE = 1001;
 
     private static final String MAIN_WIDGET_1_KEY = "main_custom_widgets1";
     private static final String MAIN_WIDGET_2_KEY = "main_custom_widgets2";
@@ -68,6 +79,7 @@ public class LockScreenClock extends SettingsPreferenceFragment implements Prefe
     private Preference mExtraWidget4;
     private Preference mDeviceInfoWidgetPref;
     private Button mApplyChange;
+    private Preference mCustomImagePreference;
     
     private SwitchPreferenceCompat mLockScreenWidgetsEnabledPref;
     private List<Preference> mWidgetPreferences;
@@ -98,6 +110,44 @@ public class LockScreenClock extends SettingsPreferenceFragment implements Prefe
         loadInitialPreferences();
         saveInitialPreferences();
         mApplyChange.setEnabled(false);
+        
+        mCustomImagePreference = findPreference(CUSTOM_IMAGE_REQUEST_CODE_KEY);
+        int clockStyle = Settings.System.getIntForUser(getContext().getContentResolver(), "clock_style", 0, UserHandle.USER_CURRENT);
+        String imagePath = Settings.System.getString(getContext().getContentResolver(), "custom_aod_image_uri");
+        if (imagePath != null && clockStyle > 0) {
+            mCustomImagePreference.setSummary(imagePath);
+            mCustomImagePreference.setEnabled(true);
+        } else if (clockStyle == 0) {
+            mCustomImagePreference.setSummary(getContext().getString(R.string.custom_aod_image_not_supported));
+            mCustomImagePreference.setEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mCustomImagePreference) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.setType("image/*");
+            startActivityForResult(intent, CUSTOM_IMAGE_REQUEST_CODE);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        super.onActivityResult(requestCode, resultCode, result);
+        if (requestCode == CUSTOM_IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK && result != null) {
+            Uri imgUri = result.getData();
+            if (imgUri != null) {
+                String savedImagePath = ImageUtils.saveImageToInternalStorage(getContext(), imgUri, "lockscreen_aod_image", "LOCKSCREEN_CUSTOM_AOD_IMAGE");
+                if (savedImagePath != null) {
+                    ContentResolver resolver = getContext().getContentResolver();
+                    Settings.System.putStringForUser(resolver, "custom_aod_image_uri", savedImagePath, UserHandle.USER_CURRENT);
+                    mCustomImagePreference.setSummary(savedImagePath);
+                }
+            }
+        }
     }
 
     private void initializePreferences() {

src/com/everest/basecamp/fragments/WallpaperDepth.java
@@ -43,16 +43,9 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import com.everest.basecamp.utils.ImageUtils;
+
 import java.util.List;
-import java.util.Locale;
 
 @SearchIndexable
 public class WallpaperDepth extends SettingsPreferenceFragment
@@ -102,50 +95,12 @@ public class WallpaperDepth extends SettingsPreferenceFragment
 
             final Uri imgUri = result.getData();
             if (imgUri != null) {
-                String savedImagePath = saveImageToInternalStorage(getContext(), imgUri);
+                String savedImagePath = ImageUtils.saveImageToInternalStorage(getContext(), imgUri, "depthwallpaper", "DEPTH_WALLPAPER_SUBJECT");
                 if (savedImagePath != null) {
                     ContentResolver resolver = getContext().getContentResolver();
                     Settings.System.putStringForUser(resolver, "depth_wallpaper_subject_image_uri", savedImagePath, UserHandle.USER_CURRENT);
                 }
             }
-        }
-    }
-
-    private String saveImageToInternalStorage(Context context, Uri imgUri) {
-        try {
-            InputStream inputStream;
-            if (imgUri.toString().startsWith("content://com.google.android.apps.photos.contentprovider")) {
-                List<String> segments = imgUri.getPathSegments();
-                if (segments.size() > 2) {
-                    String mediaUriString = URLDecoder.decode(segments.get(2), StandardCharsets.UTF_8.name());
-                    Uri mediaUri = Uri.parse(mediaUriString);
-                    inputStream = context.getContentResolver().openInputStream(mediaUri);
-                } else {
-                    throw new FileNotFoundException("Failed to parse Google Photos content URI");
-                }
-            } else {
-                inputStream = context.getContentResolver().openInputStream(imgUri);
-            }
-            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
-            String imageFileName = "DEPTH_WALLPAPER_SUBJECT_" + timeStamp + ".png";
-            File directory = new File("/sdcard/depthwallpaper");
-            if (!directory.exists() && !directory.mkdirs()) {
-                return null;
-            }
-            File[] files = directory.listFiles((dir, name) -> name.startsWith("DEPTH_WALLPAPER_SUBJECT_") && name.endsWith(".png"));
-            if (files != null) {
-                for (File file : files) {
-                    file.delete();
-                }
-            }
-            File file = new File(directory, imageFileName);
-            try (FileOutputStream outputStream = new FileOutputStream(file)) {
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-            }
-            return file.getAbsolutePath();
-        } catch (Exception e) {
-            return null;
         }
     }
 

src/com/everest/basecamp/utils/ImageUtils.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2023-2024 the risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.everest.basecamp.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class ImageUtils {
+
+    public static String saveImageToInternalStorage(Context context, Uri imgUri, String featurePath, String filePrefix) {
+        try {
+            InputStream inputStream;
+            if (imgUri.toString().startsWith("content://com.google.android.apps.photos.contentprovider")) {
+                List<String> segments = imgUri.getPathSegments();
+                if (segments.size() > 2) {
+                    String mediaUriString = URLDecoder.decode(segments.get(2), StandardCharsets.UTF_8.name());
+                    Uri mediaUri = Uri.parse(mediaUriString);
+                    inputStream = context.getContentResolver().openInputStream(mediaUri);
+                } else {
+                    throw new FileNotFoundException("Failed to parse Google Photos content URI");
+                }
+            } else {
+                inputStream = context.getContentResolver().openInputStream(imgUri);
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+            String imageFileName = filePrefix + "_" + timeStamp + ".png";
+            File directory = new File("/sdcard/risingOS/" + featurePath);
+            if (!directory.exists() && !directory.mkdirs()) {
+                return null;
+            }
+            File[] files = directory.listFiles((dir, name) -> name.startsWith(filePrefix) && name.endsWith(".png"));
+            if (files != null) {
+                for (File file : files) {
+                    file.delete();
+                }
+            }
+            File file = new File(directory, imageFileName);
+            try (FileOutputStream outputStream = new FileOutputStream(file)) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            }
+            return file.getAbsolutePath();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}